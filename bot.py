6c1e9e838a33afd82ded8efc53f58f802b5e7c5c
v1s1t0r999 <aditya.funs.11@gmail.com>
2022-02-28 18:54:31 +0530

    rainbow role cmd and multiplier while lvling up

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
-
+#fr
 
 import discord
 import ShitDB
-from discord.ext import commands
+import asyncio
+from discord.ext import commands, tasks
 from modules import global_variables as env
 
 
 
-bot = commands.Bot(command_prefix = env.prefix, intents = env.intents)
+bot = commands.Bot(command_prefix = env.prefix, intents = env.intents, strip_after_prefix=True)
 
 db = ShitDB.AsyncDB(
 	github_token = env.github_token,
@@ -30,7 +31,7 @@ async def on_ready():
 	=========
 	""")
 
-	
+
 
 @bot.event
 async def on_message(ctx):
@@ -39,20 +40,82 @@ async def on_message(ctx):
 	user_data = all_levels.get(ctx.author.id)                                # Get the user's data
 	
 	if user_data is None:                                                    # It means the bot is meeting the user for the first time
-		all_levels.update({ctx.author.id:{"msgs":0,"lvl":0}})                # Make a new data for the user with everything as 0 (start)
+		all_levels.update(
+			{
+				ctx.author.id:{"msg":0,"lvl":0,"mul":1}		# mul param is the multiplier
+			}
+		)       						         # Make a new data for the user with everything as 0 (start)
 		user_data = all_levels.get(ctx.author.id)                            # {"msgs":0,"lvl":0}
-	user_data['msgs']+=1                                                     # Increment the number of messages for the user
+	user_data['msg']+=1                                                     # Increment the number of messages for the user
 	
-	if user_data['msgs']>=10:                                                # If the nuber of messages is more than 10, goes to next level
+	if user_data['msg']>=(10*user_data['mul']):                             # If the nuber of messages is more than 10, goes to next level
 		user_data['lvl']+=1
-		user_data['msgs']=0
+		user_data['msg']=0
+		user_data['mul']+=1
+		await ctx.send(f"Bruh!! **{ctx.author}** just levelled up to level: {user_data['lvl']} WTF! ||{user_data['mul']*10} mesages for level {user_data['lvl']}!!||")
 	all_levels[ctx.author.id] = user_data                                    # Add the user's info to the main data
-	
 	await db.push_remote_data(content=all_levels, file_path="levels.json")   # Push it using: ShitDB.AsyncDB.push_remote_data()
-	await bot.process_commands(ctx)
+	return await bot.process_commands(ctx)
 
-	
-	
-	
+
+
+
+@bot.command(help="Check your fucking rank smh!",aliases=["lvl","level"])
+async def rank(ctx,user=None):
+	if not user:
+		user=ctx.author
+	all_data = await db.load_remote_data("level.json", eval_output=True)
+	user_data = all_data.get(user.id)
+	if user_data is None:
+		all_levels.update({user.id:{"msg":0,"lvl":0,"mul":1}})
+		return await ctx.send(f"User {user} just got registered in the db. Am I meeting him/her/||da gae|| for the first time??")
+	return await ctx.send(embed=discord.Embed(
+		description = f"""\
+		{user.mention}?? Ooo
+		Level: {user_data["lvl"]}
+		Needs more `{user_data["mul"]*10-user_data["msg"]}` messages for level {user_data["lvl"]+1}!!
+		"""))
+
+
+
+
+@bot.command(help="Set a rainbow role?")
+async def rainbow(ctx,role:discord.Role=None):
+	if role is None:
+		role = ctx.guild.create_role(name="Rainbow Perm!")
+	if not isinstance(role,discord.Role):
+		return await ctx.send("Mention an existing role stupid!")
+	all_guilds = await db.load_remote_data("rainbow.json", eval_output=True)
+	try:
+		r = discord.utils.get(ctx.guild.roles, id=all_guilds[ctx.guild.id]["roleid"])
+		await ctx.send(embed=discord.Embed(description=f"{r.mention} is already a rainbow role!!\nSend `1` to change; `2` for nvm"))
+		try:
+			msg = await bot.wait_for("message",check=chk,timeout=20.0)
+			if msg.content.lower()=="1":
+				await ctx.send("Ok! passed!")
+				pass
+			else:
+				return await ctx.send("Nvm!")
+		except asyncio.TimeoutError:
+			return await ctx.send(f"You didnt reply in time stupido!")
+	all_guilds.update({ctx.guild.id:{"roleid":role.id,"name":role.name}})
+	await db.push_remote_data(content=all_guilds, file_path="rainbow.json")
+	await ctx.send(embed=discord.Embed(description=f"{role.mention} is now a Rainbow role. It will change its colour in every 5 minutes!"))
+
+
+@tasks.loop(minutes=5.0)
+async def rainbow_task():
+	all_guilds = await db.load_remote_data("rainbow.json", eval_output=True) # {"guild1":{"roleid":1234,"name":"R.R"},"guild2":{"roleid":6789,"n":"rainbow!"}}
+	guild_list = list(all_guilds) # [guild1,guild2]
+	for g in guild_list:
+		# g = guild1 and guild2
+		# all_guilds["guild1" or g]["roleid"] = rainbow role id
+		r = discord.utils.get(g.roles, id=all_guilds[g]["roleid"])
+		await r.edit(colour=discord.Colour.random())
+
+
+
+
+
 bot.run(env.token)
 	
